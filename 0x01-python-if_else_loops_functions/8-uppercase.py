baa14bf698fed6f2ad27c541ca3d58acd5965eb6
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-06-28 21:16:19 +0000

    Add task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def make_upper(letter):
     if 96 < ord(letter) < 123:
-        return(chr(ord(letter)- 32))
+        return(ord(letter)- 32)
     else:
-        return(chr(ord(letter)))
+        return(ord(letter))
 
 def uppercase(str):
     upper_string = ""
     for letter in str:
-        upper_string += "{}".format(letter)
+        upper_string = "{}".format(letter)
     print({}.format(upper_string))
