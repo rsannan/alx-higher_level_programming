781c43a157310236e35d46869f64392b4fd6b150
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-06-28 19:26:20 +0000

    Add task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number1 in range(0,  10):
+    for number2 in range(0, 10):
+        if (number1 * 10) + number2 == 89:
+            print("{}".format((number1 * 10) + number2))
+        if number1 < number2:
+            print("{:02d}".format((number1 * 10) + number2), end=", ")
