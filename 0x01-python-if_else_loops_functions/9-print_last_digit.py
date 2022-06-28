c8c510a09cd93d18d64b50d0374198006d43f2fa
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-06-28 21:35:21 +0000

    Add task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        new_number = number * -1
+    else:
+        new_number = number
+
+    last_digit = new_number % 10
+    print("{:d}".format(last_digit))
+    return(last_digit)
