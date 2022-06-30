af2ee18905ca1d14106de4ff9a3b357b0e43f14a
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-06-30 17:09:01 +0000

    Add task 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ import calculator_1 as calc
 if __name__ == "__main__":
     a = 10
     b = 5
-    add_total = calc.add(a, b)
-    sub_total = calc.sub(a, b)
-    mul_total = calc.mul(a, b)
-    div_total = calc.div(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, add_total))
-    print("{:d} - {:d} = {:d}".format(a, b, sub_total))
-    print("{:d} * {:d} = {:d}".format(a, b, mul_total))
-    print("{:d} / {:d} = {:d}".format(a, b, div_total))
+    print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
