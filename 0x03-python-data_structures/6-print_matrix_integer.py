62a6b53a7266813ae4d10dd67095bbefbc535b2d
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-03 01:21:31 +0000

    fix task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        for l in range(len(matrix[i])):
-            if matrix[i][-1] != matrix[i][l]:
-                print("{:d}".format(matrix[i][l]), end="")
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
                 print(" ", end="")
-            else:
-                print("{:d}".format(matrix[i][l]))
+
+        print("")
