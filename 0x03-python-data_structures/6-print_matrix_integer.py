1cb0c125f7e225212853becfe2818d8e1c5ecfba
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-03 00:42:55 +0000

    Fix task 9

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if isinstance(matrix, list):
-        for mat_list in matrix:
-            if isinstance(mat_list, list):
-                if len(mat_list) == 0:
-                    continue
-                for number in mat_list:
-                    if mat_list[-1] == number:
-                        print("{:d}".format(number),)
-                    else:
-                        print("{:d}".format(number), end=" ")
+    for mat_list in matrix:
+        if len(mat_list) == 0:
+            continue
+        for number in mat_list:
+            if mat_list[-1] == number:
+                print("{:d}".format(number))
+            else:
+                print("{:d}".format(number), end=" ")
