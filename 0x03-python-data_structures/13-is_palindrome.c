1e3f1fd771fe787f7dd06c157fe8edc6270d4ac2
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-04 19:23:34 +0000

    Fix task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -53,8 +53,6 @@ return (count);
 int is_palindrome(listint_t **head)
 {
 	int list_len = listint_len(*head);
-	if(list_len == 0)
-		return(1);
 	int i_bottom = list_len - 1;
 	int i_top = 0;
 	return(check_palindrome(i_top, i_bottom, list_len, *head));
