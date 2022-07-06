f40da60a086909e1cccef74c20f5e8595e8f0f13
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-06 19:24:06 +0000

    Add task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                  'D': 500, 'M': 1000}
+    if isinstance(roman_string, str) and roman_string is not None:
+        for letter in range(len(roman_string)):
+            c_letter = roman_dict.get(roman_string[letter])
+            if letter != 0:
+                p_letter = roman_dict.get(roman_string[letter-1])
+                if c_letter > p_letter:
+                    sum_letter = c_letter - p_letter
+                else:
+                    sum_letter = sum_letter + c_letter
+
+            else:
+                sum_letter = c_letter
+
+        return sum_letter
+    return 0
