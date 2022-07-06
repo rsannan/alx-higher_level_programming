f7cbed2d1f76dd7721e4aa3570b0d218fd0bc01e
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-06 19:52:36 +0000

    Fix task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,21 +2,25 @@
 def roman_to_int(roman_string):
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                   'D': 500, 'M': 1000}
+    sum_letter = 0
     if isinstance(roman_string, str) and roman_string is not None:
         for letter in range(len(roman_string)):
-            if roman_string[letter] not in roman_dict:
+            if roman_string[letter].upper() not in roman_dict:
                 sum_letter = sum_letter + 0
                 continue
-            c_letter = roman_dict.get(roman_string[letter])
-            if letter != 0:
-                p_letter = roman_dict.get(roman_string[letter-1])
+
+            c_letter = roman_dict.get(roman_string[letter].upper())
+
+            if letter == 0:
+                sum_letter = c_letter
+
+            else:
+                p_letter = roman_dict.get(roman_string[letter-1].upper())
                 if c_letter > p_letter:
                     sum_letter = c_letter - p_letter
                 else:
                     sum_letter = sum_letter + c_letter
 
-            else:
-                sum_letter = c_letter
 
         return sum_letter
     return 0
