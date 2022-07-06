6d6939c7a778c42c24689ba93bd69148a220eec3
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-06 18:20:57 +0000

    Add task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary :
+        return
+
+    large_num = 0 
+    for key, value in a_dictionary.items():
+        if value > large_num:
+            dict_tup = (key, value)
+            large_num = value
+
+    return dict_tup[0]
+
