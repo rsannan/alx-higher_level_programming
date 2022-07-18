c2c2607b0e644e2fbd8b47462735746e7c2dec92
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-18 14:16:25 +0000

    Add task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for item in range(x):
+        try:
+            print("{:d}".format(my_list[item]), end="")
+            count += 1
+
+        except (TypeError, ValueError):
+            continue
+
+        except IndexError:
+            print("", end='\n')
+            return count
+
+        print("", end='\n')
+        return count
