9df6cd692fe1328e4c12f44d389fc8bd04067477
rsannan <shawnrannan@gmail.com>
2022-07-19 16:32:03 +0000

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This defines a Square class"""
+
+
+class Square:
+    """Square class definition"""
+    def __init__(self, size):
+        """Initializes data"""
+        self.size = __size
