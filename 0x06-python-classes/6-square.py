9e91e1e1844bdc2772f48e085c987d16ac166fd2
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-19 21:26:17 +0000

    Add task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,7 +48,7 @@ class Square:
             print("")
         else:
             for i in range(self.__size):
-                if self.__position[1] <= self.__position[0]:
+                if self.__position[1] < 0:
                     for k in range(self.__position[0]):
                         print(" ", end="")
                 for j in range(self.__size):
