53891e2222947cd3beefa72ef911ae2b7ed5b123
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-19 20:38:07 +0000

    Add task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This defines a Square class"""
+
+
+class Square:
+    """Square class definition"""
+    def __init__(self, size=0):
+        """Initializes the data
+           size(int): Size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the Area of the Square"""
+        return self.__size ** 2
