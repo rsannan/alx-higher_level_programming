5444c1c170f91794bee5b5b472476d79ac8a5315
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-20 22:09:50 +0000

    Add optional tasks

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,7 +6,8 @@ import sys
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-        return (result)
-    except:
+        return(result)
+
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return(None)
