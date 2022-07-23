413e7edac25a58ac774b64daecf2b29502ca8291
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-23 21:44:08 +0000

    Add test for task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -17,12 +17,9 @@ def say_my_name(first_name, last_name=""):
     elif not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
 
-    f_len = len(first_name)
     l_len = len(last_name)
-    if f_len == 0 and l_len == 0:
-        print("")
 
-    elif l_len == 0:
+    if l_len == 0:
         print("My name is {}".format(first_name))
 
     else:
