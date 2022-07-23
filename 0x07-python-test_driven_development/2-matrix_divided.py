c8591ff96c752c8fbb09ac907209f8bdc1aeb9b0
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-07-23 21:23:02 +0000

    Add tests for tasks 0-2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,16 +12,28 @@ def matrix_divided(matrix, div):
             a nested list of divided elements
             TypeError, ZeroDivisionError Otherwise
     """
-    bool_mat= not isinstance(matrix, list)
+def matrix_divided(matrix, div):
+    bool_mat = not isinstance(matrix, list)
     bool_matlist = not all(isinstance(element, list) for element in matrix)
+    if (bool_mat or bool_matlist):
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    
     matele = [matrix[i][j] for i in range(len(matrix)) for j in range(len(matrix[i]))]
     bool_matele = not all(isinstance(num, (int, float)) for num in matele)
 
-    if (bool_mat or bool_matlist or bool_matele):
+    if (bool_matele):
             raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
     new_matrix = []
+    
     for i in range(len(matrix)):
         list_matrix = []
+        if i == 0:
+            match_matrix = len(matrix[i])
+        else: 
+            match_matrix = len(matrix[i- 1])
+            
+        if match_matrix != len(matrix[i]):
+            raise TypeError('Each row of the matrix must have the same size')
         for j in range(len(matrix[i])):
             if not isinstance(div, (int, float)):
                 raise TypeError('div must be a number')
@@ -30,5 +42,5 @@ def matrix_divided(matrix, div):
             else:
                 number = matrix[i][j] / div
                 list_matrix.append(round(number, 2))
-        new_matrix.append(list_matrix)
+        new_matrix.append(list_matrix)  
     return new_matrix
