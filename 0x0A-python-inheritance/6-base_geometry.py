a353aec25206a1851bedcdb031c386739ec35c8e
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-08-01 21:14:45 +0000

    Add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains BaseGeometry class"""
+
+
+class BaseGeometry():
+    """This is a geometry class"""
+    def __init__(self):
+        """initializing the class"""
+        pass
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
