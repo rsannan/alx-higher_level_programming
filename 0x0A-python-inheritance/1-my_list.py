e4e41913fe1680c6d0da2e9b4cb34f850394de25
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-08-01 15:39:49 +0000

    Add task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""This module defines the MyList class"""
-
-
 class MyList(list):
-    """definition of a list subclass"""
     def print_sorted(self):
-        """prints a sorted Mylist"""
-        sort = self
-        print(sorted(sort))
+        a = self
+        print(sorted(a))
