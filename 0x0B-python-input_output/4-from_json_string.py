a3d29d2665c743ef4352272a39053f4689aab390
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-08-02 14:59:49 +0000

    Add task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines the from_json_string function"""
+import json
+
+
+def from_json_string(my_str):
+    """ Returns an object represented by a JSON string
+        Args:
+            my_str (str): JSON string
+    """
+    return json.loads(my_str)
