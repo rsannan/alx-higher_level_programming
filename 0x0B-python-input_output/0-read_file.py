b1d9bd3c5a934e523b6cfdf8530369cb1b7e8ba8
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-08-02 14:11:04 +0000

    Add task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines the read_file function"""
+
+
+def read_file(filename=""):
+    """ Reads a file
+        Args:
+            filename (str): file to read from
+    """
+    with open(filename, encoding="utf-8") as myfile:
+        return myfile
