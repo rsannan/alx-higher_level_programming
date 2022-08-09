63fcc16ed218d58265d00c3a8708cbb501802eb2
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-08-09 01:10:05 +0000

    Fix base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,13 +35,11 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Returns the JSON representation of list_dictionaries
-
+        """Return the JSON serialization of a list of dicts.
         Args:
-            list_dictionaries (list): a list of dictionaries
+            list_dictionaries (list): A list of dictionaries.
         """
-        if (list_dictionaries == [] or
-           list_dictionaries is None):
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
