2fbbf7c91b46a740618bf01b27c4f74651610f90
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-09-08 02:06:53 +0000

    Add task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- lists all genres of the show Dexter
 
-SELECT tv_genres.name AS name
+SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id=8
-ORDER BY name ASC;
+ORDER BY tv_genres.name ASC;
