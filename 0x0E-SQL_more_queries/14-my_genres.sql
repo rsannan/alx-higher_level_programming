c455e40450e1dd754272d74bc306cb908e334a73
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-09-08 02:12:58 +0000

    Fix task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,6 +5,6 @@ FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
-ON tv_shows.id = tv_shows_genres.show_id
+ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title="Dexter"
 ORDER BY tv_genres.name ASC;
