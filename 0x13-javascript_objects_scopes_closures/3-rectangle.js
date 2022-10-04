80f0226f12ca99ab8bf391b5b2b6a4a551c00b4e
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-10-04 16:47:15 +0000

    Add task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { this.width = w; this.height = h; }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let l = 0; l < this.width; l++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+};
