b075f585b960622e9dc1878a74387b6646516706
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-10-06 23:25:09 +0000

    Add relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,9 @@
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY state.id")
+    result = mycursor.fetchall()
+    for x in myresult:
+        print(x)
