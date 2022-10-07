90082fb30c5e79119e0e7e00179471b148f324f7
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-10-07 17:42:40 +0000

    Fix task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> \
-                            <mysql password> \
-                             <database name>
-"""
-import MySQLdb
-from sys import argv
-
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states\
-               WHERE name = '{:s}' ORDER BY id".format(argv[4]))
-    result = cur.fetchall()
-    for x in result:
-        if x[1] == argv[4]:
-            print(x)
-    cur.close()
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(user=sys.argv[1],
+                         host="localhost",
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    myCursor = db.cursor()
+    myCursor.execute("SELECT cities.id, cities.name, states.name\
+                    FROM cities JOIN states ON\
+                    states.id = cities.state_id ORDER BY cities.id ASC")
+    for i in myCursor.fetchall():
+        print(i)
     db.close()
