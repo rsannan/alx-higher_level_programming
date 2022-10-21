2a54c2e61a7e35c70a533a950bae306fdf3f6773
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-10-21 23:14:07 +0000

    Add task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL
+displays the body of the response
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
+    else:
+        print(r.text)
