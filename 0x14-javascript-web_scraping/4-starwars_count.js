9d845d0207db41d96d7955e0562bfead498e0b3b
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-11-15 18:04:39 +0000

    Add task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// prints the number of movies
+// where the character “Wedge Antilles” is present
+
+const request = require('request');
+const myArgs = process.argv.slice(2);
+let count = 0;
+
+request(myArgs[0], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const allFilms = JSON.parse(body).results;
+    for (const film of allFilms) {
+      const characters = film.characters;
+      for (const val of characters) {
+        if (val === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count += 1;
+        }
+      }
+    }
+  }
+  console.log(count);
+});
